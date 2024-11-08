9396dfcd06f76d03b371e2ad4505e0a30be23345
hhlitval <litvinhh@gmail.com>
2024-11-08 16:32:42 +0100

    Add mobile navigation for header

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-const nav = document.querySelector(".nav");
-const navBtn = document.querySelector(".nav-burger");
+const burgerMenu = document.querySelector(".nav-burger"),
+  nav = document.querySelector(".nav"),
+  navList = document.querySelector(".header-nav__list"),
+  currentYear = document.querySelector(".year");
 
-navBtn.onclick = () => {
+currentYear.textContent = new Date().getFullYear();
+
+window.addEventListener("scroll", function () {
+  const wrapper = document.querySelector(".wrapper"),
+    header = document.querySelector(".header");
+  let triggerHeight = wrapper.offsetHeight - 100;
+
+  if (window.scrollY > triggerHeight) {
+    header.classList.add("header-sticky");
+    wrapper.classList.add("sticky");
+  } else {
+    header.classList.remove("header-sticky");
+    wrapper.classList.remove("sticky");
+  }
+});
+
+burgerMenu.addEventListener("click", () => {
   nav.classList.toggle("mobile");
-};
+});
